1027979d775fba33424e9966bce096526068760c
Saw Soe <sawsoemoenyunt@connect.qut.edu.au>
2021-05-12 17:57:40 +1000

    Update FeedListViewCell.swift

--- FeedApp/Views/FeedListViewCell.swift
+++ FeedApp/Views/FeedListViewCell.swift
@@ -9,4 +9,16 @@ import LBTATools
 
 class FeedListViewCell: UICollectionViewCell {
     
+    var imageView = UIImageView(backgroundColor: .blue)
+    var titleLabel = UILabel(text: "Sample text", font: .boldSystemFont(ofSize: 14), textColor: .black, textAlignment: .left, numberOfLines: 2)
+    var bodyLabel = UILabel(text: "Boday text...", font: .systemFont(ofSize: 12), textColor: .black, textAlignment: .left, numberOfLines: 4)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
