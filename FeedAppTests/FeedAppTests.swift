9e96643171fc1c799d95a6e3f40e5487d8a08571
Saw Soe <sawsoemoenyunt@connect.qut.edu.au>
2021-05-12 21:59:05 +1000

    Update FeedAppTests.swift
    test added

--- FeedAppTests/FeedAppTests.swift
+++ FeedAppTests/FeedAppTests.swift
@@ -7,8 +7,35 @@
 
 import XCTest
 @testable import FeedApp
+import SwiftyJSON
 
 class FeedAppTests: XCTestCase {
+    
+    
+    func testFeedObject() throws {
+        let feed = Feed(userID: 1, id: 1, title: "feed title", body: "feed body", imageUrl: "imageUrl", thumbnailUrl: "thumbUrl")
+        
+        XCTAssertEqual(feed.title, "feed title")
+    }
+    
+    func testFeedFromJSON() throws {
+        if let path = Bundle.main.path(forResource: "response", ofType: "json") {
+            do {
+                
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = JSON(data)
+                
+                let feed:Feed = Feed()
+                feed.fromJSON(jsonData: jsonResult.arrayValue[0])
+                
+                XCTAssertEqual(feed.id, 1)
+                
+            } catch {
+                print("Failed to load data from json file")
+            }
+        }
+    }
+    
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
