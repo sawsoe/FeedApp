3f9097bcff463b7795495b15dc93109e56c6c9e1
Saw Soe <sawsoemoenyunt@connect.qut.edu.au>
2021-05-12 18:03:04 +1000

    Update FeedListCollectionViewController.swift

--- FeedApp/Controllers/FeedListCollectionViewController.swift
+++ FeedApp/Controllers/FeedListCollectionViewController.swift
@@ -8,6 +8,30 @@
 import LBTATools
 
 class FeedListCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let cellID = "feedListCellID"
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.register(FeedListViewCell.self, forCellWithReuseIdentifier: cellID)
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as? FeedListViewCell
+        return cell!
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 300)
+    }
     
 }
