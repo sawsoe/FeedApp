6dac18eb5a967c44b11e4866081cfcfcc04c7f06
Saw Soe <sawsoemoenyunt@connect.qut.edu.au>
2021-05-12 17:50:47 +1000

    Create FeedListCollectionViewController

---
+++ FeedApp/Controllers/FeedListCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FeedListCollectionViewController.swift
+//  FeedApp
+//
+//  Created by SawSMN's MacBook Pro on 12/5/21.
+//
+
+import LBTATools
+
+class FeedListCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    
+}
