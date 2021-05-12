f37b310f14d36a3461bf094f0bc4e24f1698db57
Saw Soe <sawsoemoenyunt@connect.qut.edu.au>
2021-05-12 19:09:45 +1000

    update Feed class

--- FeedApp/Models/Feed.swift
+++ FeedApp/Models/Feed.swift
@@ -6,18 +6,36 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 class Feed {
-    var userID : Int
-    var id : Int
-    var title : String
-    var body : String
+    var userID : Int?
+    var id : Int?
+    var title : String?
+    var body : String?
+    var imageUrl: String?
+    var thumbnailUrl: String?
     
-    init(userID:Int, id:Int, title:String, body:String) {
+    init(userID:Int, id:Int, title:String, body:String, imageUrl:String, thumbnailUrl:String) {
         self.userID = userID
         self.id = id
         self.title = title
         self.body = body
+        self.imageUrl = imageUrl
+        self.thumbnailUrl = thumbnailUrl
         
+    }
+    
+    init() {
+        
+    }
+    
+    func fromJSON(jsonData:JSON){
+        self.userID = jsonData["userID"].intValue
+        self.id = jsonData["id"].intValue
+        self.title = jsonData["title"].stringValue
+        self.body = jsonData["body"].stringValue
+        self.imageUrl = jsonData["imageUrl"].stringValue
+        self.thumbnailUrl = jsonData["thumbnailUrl"].stringValue
     }
 }
