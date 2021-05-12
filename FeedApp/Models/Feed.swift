96110ff01e6c02b90ae04e9a5ba77c60384c43cc
Saw Soe <sawsoemoenyunt@connect.qut.edu.au>
2021-05-12 21:39:56 +1000

    Update Feed.swift
    Documentation added

--- FeedApp/Models/Feed.swift
+++ FeedApp/Models/Feed.swift
@@ -8,14 +8,35 @@
 import Foundation
 import SwiftyJSON
 
+///Content to show in News Feed
 class Feed {
+    /// The userID of the Feed.
     var userID : Int?
+    /// The id of the Feed.
     var id : Int?
+    /// The title of the Feed.
     var title : String?
+    /// The body of the Feed.
     var body : String?
+    /// The imageUrl of the Feed.
     var imageUrl: String?
+    /// The thumbnailUrl of the Feed.
     var thumbnailUrl: String?
     
+    /**
+         Initializes a new feed with the provided informations.
+
+         - Parameters:
+            - userID: The userID of the Feed
+            - id: The id of the Feed
+            - title: The title of the Feed
+            - body: The body of the Feed
+            - imageUrl: The imageUrl of the Feed
+            - thumbnailUrl: The thumbnailUrl of the Feed
+     
+
+         - Returns: A Feed
+         */
     init(userID:Int, id:Int, title:String, body:String, imageUrl:String, thumbnailUrl:String) {
         self.userID = userID
         self.id = id
@@ -26,10 +47,24 @@ class Feed {
         
     }
     
+    /**
+         Initializes a new feed without any informations.
+
+         - Returns: An Empty Feed content
+         */
     init() {
         
     }
     
+    /**
+         Initializes a new feed from JSON object.
+
+         - Parameters:
+            - jsonData: The JSON object
+     
+
+         - Returns: A Feed content
+         */
     func fromJSON(jsonData:JSON){
         self.userID = jsonData["userID"].intValue
         self.id = jsonData["id"].intValue
